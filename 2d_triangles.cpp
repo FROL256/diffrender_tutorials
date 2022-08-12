87f52b3e516ea938ed3f7f975ea4f4cb2375887e
Vladimir Frolov <vova@frolov.pp.ru>
2022-08-12 16:23:20 +0300

    FIX! Almost works

--- 2d_triangles.cpp
+++ 2d_triangles.cpp
@@ -45,7 +45,7 @@ using LiteMath::int2;
 using LiteMath::clamp;
 using LiteMath::normalize;
 
-#define DEBUG_RENDER 0
+#define DEBUG_RENDER 1
 
 constexpr static int  SAM_PER_PIXEL = 16;
 constexpr static int  MAXTHREADS    = 14;
@@ -281,7 +281,7 @@ void compute_interior_derivatives(const TriangleMesh &mesh,
               const float dF_dU = dot((c0-c2), val);
               const float dF_dV = dot((c1-c2), val);
               
-              if(dF_dU > 0.0f || dF_dV > 0.0f) 
+              if(dF_dU*dF_dU > 0.0f || dF_dV*dF_dV > 0.0f) 
               {
                 const float3 v0 = mesh.vertices[A];
                 const float3 v1 = mesh.vertices[B];
@@ -603,8 +603,8 @@ int main(int argc, char *argv[])
   //scn01_TwoTrisFlat(initialMesh, targetMesh);
   //scn02_TwoTrisSmooth(initialMesh, targetMesh);
   //scn03_Triangle3D_White(initialMesh, targetMesh);
-  //scn04_Triangle3D_Colored(initialMesh, targetMesh);
-  scn05_Pyramid3D(initialMesh, targetMesh);
+  scn04_Triangle3D_Colored(initialMesh, targetMesh);
+  //scn05_Pyramid3D(initialMesh, targetMesh);
 
   //g_tracer = MakeRayTracer2D("");  
   g_tracer = MakeRayTracer3D("");
@@ -620,7 +620,7 @@ int main(int argc, char *argv[])
     //return 0;
   }
 
-  if(0) // check gradients with finite difference method
+  if(1) // check gradients with finite difference method
   {
     Img target(img.width(), img.height(), float3{0, 0, 0});
     Img adjoint(img.width(), img.height(), float3{0, 0, 0});
